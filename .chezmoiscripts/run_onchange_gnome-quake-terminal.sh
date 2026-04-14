358a092ddd2fef2b0004453068a8d7d84215aaf6
xiupos <me@xiupos.net>
2026-04-15 00:22:58 +0900

    update run_onchange_gnome-quake-terminal.sh

--- .chezmoiscripts/run_onchange_gnome-quake-terminal.sh
+++ .chezmoiscripts/run_onchange_gnome-quake-terminal.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-# hash: {{ include "private_dot_local/share/gnome-extensions-settings/quake-terminal.dconf" | sha256sum }}
+# hash: {{ include "private_dot_local/private_share/gnome-extensions-settings/quake-terminal.dconf" | sha256sum }}
 dconf load /org/gnome/shell/extensions/quake-terminal/ \
   < "${HOME}/.local/share/gnome-extensions-settings/quake-terminal.dconf"
