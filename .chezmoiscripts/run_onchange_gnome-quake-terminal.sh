b4c8d5122c17b59f5bbf9bc48b82167c639da927
xiupos <me@xiupos.net>
2026-05-31 12:57:49 +0900

    update run_onchange_gnome-quake-terminal.sh

--- .chezmoiscripts/run_onchange_gnome-quake-terminal.sh
+++ .chezmoiscripts/run_onchange_gnome-quake-terminal.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 # hash: {{ $f := "private_dot_local/private_share/gnome-extensions-settings/quake-terminal.dconf" }}{{ if stat (joinPath .chezmoi.sourceDir $f) }}{{ include "private_dot_local/private_share/gnome-extensions-settings/quake-terminal.dconf" | sha256sum }}{{ else }}no-file{{ end }}
-dconf load /org/gnome/shell/extensions/quake-terminal/ \
-  < "${HOME}/.local/share/gnome-extensions-settings/quake-terminal.dconf"
+
+target="${HOME}/.local/share/gnome-extensions-settings/quake-terminal.dconf"
+
+if [[ ! -f "${target}" ]]; then
+  echo "quake-terminal.dconf not found, skipping." >&2
+  exit 0
+fi
+
+dconf load /org/gnome/shell/extensions/quake-terminal/ < "${target}"
